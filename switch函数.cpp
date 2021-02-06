3147382ca95f934b241f3fb42533138a72316e60
zxqcht <78062818+zxqcht@users.noreply.github.com>
2021-02-06 11:22:58 +0800

    Add files via upload

---
+++ switch函数.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{ int a;
+   printf("Please input a number:");
+   scanf("%d",&a);
+   switch(a)
+   { case 1:printf("I "); 
+     case 2:printf("love ");
+     case 3:printf("you ");
+     case 4:printf("forever ");
+     case 5:printf("zhang ");
+     case 6:printf("xue ");
+     case 7:printf("qi ");
+	} 
+    
+    return 0;
+}                
